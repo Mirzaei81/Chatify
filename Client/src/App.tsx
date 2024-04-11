dda274e7b2fdd3e1d2ec2481594bc817fead6212
Mirzaei81 <pc@MtheBest>
2024-04-11 09:12:30 +0330

    modified:   src/App.css modified:   src/App.tsx 	new file:   src/Hooks/wsHooks.tsx 	modified:   src/index.css 	modified:   src/main.tsx 	modified:   tailwind.config.js

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,32 +1,20 @@
-import { useEffect } from 'react'
+import { useEffect,useRef } from 'react'
 import './App.css'
-import useWebSocket, { ReadyState } from 'react-use-websocket';
 import { useState,useCallback } from 'react';
 import { IoArrowForward } from "react-icons/io5";
-import toast, { Toast, Toaster } from 'react-hot-toast';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
+import toast from 'react-hot-toast';
 
-const wsURL = "ws://127.0.0.1:8000/ws/chat/lobby/"
-function App() {
 
   //Public API that will echo messages sent to it back to the client
+let wsURL = "ws://127.0.0.1:8000/ws/chat/lobby/"
+
+function App() {
+  const [val,setVal] =useState("")
   const [socketUrl, setSocketUrl] = useState(wsURL);
   const [messageHistory, setMessageHistory] = useState<MessageEvent<any>[]>([]);
-  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
-
-  useEffect(() => {
-    toast.error(connectionStatus);
-    if (lastMessage !== null) {
-      setMessageHistory((prev) => prev.concat(lastMessage));
-    }
-  }, [lastMessage]);
-
-  const handleClickChangeSocketUrl = useCallback(
-    () => setSocketUrl(wsURL),
-    []
-  );
-
-  const handleClickSendMessage = useCallback(() => sendMessage('Hello'), []);
-
+  const {sendMessage,lastMessage, readyState} = useWebSocket(socketUrl);
+  
   const connectionStatus = {
     [ReadyState.CONNECTING]: 'Connecting',
     [ReadyState.OPEN]: 'Open',
@@ -35,26 +23,77 @@ function App() {
     [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
   }[readyState];
 
+  const stateChanged = ()=>{
+    return new Promise((resolve,reject)=>{
+      const intervalId = setInterval(() => {
+        if (readyState === ReadyState.OPEN) {
+          clearInterval(intervalId); // Stop the interval
+          resolve(readyState);
+          console.log(readyState);
+        } else if (readyState === ReadyState.CLOSED) {
+          clearInterval(intervalId); // Stop the interval
+          reject(readyState);
+          console.log(readyState);
+        }
+      },1000);
+    })
+  }
+
+  useEffect(() => {
+    toast.promise(
+      stateChanged(),
+      {
+        loading:"Connecting...",
+        success:<b>Connected</b>,
+        error:<b>Failed</b>
+      },
+      {
+        success: {
+          duration: 5000,
+          icon: '🔥',
+        },
+        error: {
+          duration: 5000,
+          icon: '😕',
+        },
+    }
+    )
+    
+    if (lastMessage !== null) {
+      setMessageHistory((prev) => prev.concat(lastMessage!));
+    }
+  }, [lastMessage]);
+
+  const handleClickChangeSocketUrl = useCallback(
+    () => setSocketUrl(wsURL),
+    []
+  );
+
+  const handleClickSendMessage = useCallback(()=>sendMessage(val), []);
+
   return (
-    <div className=''>
-      <Toaster/>
-      <button onClick={handleClickChangeSocketUrl}>
-        Click Me to change Socket Url
-      </button>
-      <button
-      className='w-100% rounded-sm'
-        onClick={handleClickSendMessage}
-        disabled={readyState !== ReadyState.OPEN}
-      >
-        <IoArrowForward/>
-      </button>
-      <span>The WebSocket is currently {connectionStatus}</span>
-      {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
-      <ul>
-        {messageHistory.map((message, idx) => (
-          <span key={idx}>{message ? message.data : null}</span>
-        ))}
-      </ul>
+    <div className='flex min-h-screen flex-col'>
+      <div className='bg-white grow h-full rounded'>
+        {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
+        <ul>
+          {messageHistory.map((message, idx) => (
+            <span key={idx}>{message ? message.data : null}</span>
+          ))}
+        </ul>
+      </div>
+      <div className='flex-end bg- '>
+
+        <div className='flex mb-2 w-screen  mr-2 bottom-0 fixed'>
+          <input className='grow rounded-lg' value={val} onChange={(e) => setVal(e.target.value)} />
+          <button
+            className='rounded-lg '
+            onClick={handleClickSendMessage}
+            disabled={readyState !== ReadyState.OPEN }
+          >
+            <IoArrowForward className='' />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+      <Toaster
+        position='bottom-right'
+        reverseOrder={false} 
+      />
   </React.StrictMode>,
 )
